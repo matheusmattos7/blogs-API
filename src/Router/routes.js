29d9c523373db7f3532a8c2e11b58a038535661e
matheusmattos7 <matheusm.mattos7@gmail.com>
2022-07-21 18:53:21 -0300

    feat: Adiciona rota POST /user

--- src/Router/routes.js
+++ src/Router/routes.js
@@ -3,15 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 const loginController = require('../controllers/loginController');
-// const userController = require('../controllers/userController');
+const userController = require('../controllers/userController');
 
 const { validateLogin } = require('../middlewares/validateLogin');
-// const { validateUser } = require('../middlewares/validateUser');
+const { validateUser } = require('../middlewares/validateUser');
 
 router
   .post('/login', validateLogin, loginController.login);
 
-// router
-//   .post('/user', validateUser, userController.createUser);
+router
+  .post('/user', validateUser, userController.createUser);
 
 module.exports = router;
