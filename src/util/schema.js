d9e61e2bd490359e7d68417c5d543a8cc15a7857
matheusmattos7 <matheusm.mattos7@gmail.com>
2022-07-21 18:52:18 -0300

    feat: Adiciona userSchema  para os schemas do Joi

--- src/util/schema.js
+++ src/util/schema.js
@@ -2,6 +2,9 @@ const Joi = require('joi');
 const message = require('./codeMessages');
 const status = require('./statusHttpCode');
 
+const MIN_NAME = 8;
+const MIN_PASSWORD = 6;
+
 const loginSchema = Joi.object({
   email: Joi.string().email().empty().messages({
     'string.empty': `${status.BAD_REQUEST}+${message.SOME_REQUIRED}`,
@@ -10,10 +13,27 @@ const loginSchema = Joi.object({
   password: Joi.string().empty().messages({
     'string.empty': `${status.BAD_REQUEST}+${message.SOME_REQUIRED}`,
   }).required(),
-}).messages({
+  }).messages({
   'string.empty': `${status.BAD_REQUEST}+${message.INVALID_FIELD}`,
+});
+
+const userSchema = Joi.object({
+  displayName: Joi.string().min(MIN_NAME).messages({
+    'string.min': `${status.BAD_REQUEST}+${message.DISPLAY_NAME_MIN}`,
+  })
+    .required(),
+  email: Joi.string().email().messages({
+    'string.email': `${status.BAD_REQUEST}+${message.EMAIL_VALID}`,
+  })
+    .required(),
+  password: Joi.string().min(MIN_PASSWORD).messages({
+    'string.min': `${status.BAD_REQUEST}+${message.PASSWORD_MIN}`,
+  })
+    .required(),
+  image: Joi.string().required(),
 });
 
 module.exports = {
   loginSchema,
+  userSchema,
 };
