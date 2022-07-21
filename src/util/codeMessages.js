43b0972e725847a0d2fc7d9dd246dc2691bdb978
matheusmattos7 <matheusm.mattos7@gmail.com>
2022-07-21 18:50:05 -0300

    feat: adiciona nova mensagens em codeMessages.js

--- src/util/codeMessages.js
+++ src/util/codeMessages.js
@@ -1,6 +1,10 @@
 const message = {
   SOME_REQUIRED: 'Some required fields are missing',
   INVALID_FIELD: 'Invalid fields',
+  DISPLAY_NAME_MIN: '"displayName" length must be at least 8 characters long',
+  EMAIL_VALID: '"email" must be a valid email',
+  PASSWORD_MIN: '"password" length must be at least 6 characters long',
+  REGISTERED_USER: 'User already registered',
 };
 
 module.exports = message;
