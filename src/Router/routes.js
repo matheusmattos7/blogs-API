9d9e39c1a3b9856ac5a3e699ed7cf8a418ce7df5
matheusmattos7 <matheusm.mattos7@gmail.com>
2022-07-21 20:39:07 -0300

    feat: Adiciona rota GET /user

--- src/Router/routes.js
+++ src/Router/routes.js
@@ -7,11 +7,15 @@ const userController = require('../controllers/userController');
 
 const { validateLogin } = require('../middlewares/validateLogin');
 const { validateUser } = require('../middlewares/validateUser');
+const { validateToken } = require('../middlewares/validateToken');
 
 router
   .post('/login', validateLogin, loginController.login);
 
 router
   .post('/user', validateUser, userController.createUser);
+
+router
+  .get('/user', validateToken, userController.getUser);
 
 module.exports = router;
