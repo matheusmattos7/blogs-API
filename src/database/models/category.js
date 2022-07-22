ce11ee3f549b632025ede9eb5692ec7690ab116c
matheusmattos7 <matheusm.mattos7@gmail.com>
2022-07-21 22:01:19 -0300

    feat: Cria Category model

---
+++ src/database/models/category.js
@@ -0,0 +1,19 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    tableName: 'Categories',
+    timestamps: false,
+  });
+
+  return Category;
+};
+
+module.exports = Category;
