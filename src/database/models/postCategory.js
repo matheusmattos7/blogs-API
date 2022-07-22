6bf6386b3cba22686b1c138b6f1e8c07f12070b3
matheusmattos7 <matheusm.mattos7@gmail.com>
2022-07-22 00:27:38 -0300

    feat: Adiciona postCategory models

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,38 @@
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategories = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true
+    }
+  },
+  {
+    tableName: 'PostsCategories',
+    timestamps: false,
+  });
+
+  PostCategories.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      through: PostCategories,
+      as: 'blogPosts',
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategories,
+      as: 'categories',
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    })
+  }
+
+  return PostCategories;
+};
+
+module.exports = PostCategory;
