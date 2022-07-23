6e2fd06a1a634473cf53387e9a16a14936b09e40
matheusmattos7 <matheusm.mattos7@gmail.com>
2022-07-23 01:48:10 -0300

    feat: Adiciona rota POST /post

--- src/Router/routes.js
+++ src/Router/routes.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const loginController = require('../controllers/loginController');
 const userController = require('../controllers/userController');
 const categoriesController = require('../controllers/categoriesController');
+const postController = require('../controllers/postController');
 
 const { validateLogin } = require('../middlewares/validateLogin');
 const { validateUser } = require('../middlewares/validateUser');
 const { validateToken } = require('../middlewares/validateToken');
 const { validateCategory } = require('../middlewares/validateCategory');
+const { validateBlogPost } = require('../middlewares/validateBlogpost');
 
 router
   .post('/login', validateLogin, loginController.login);
@@ -28,5 +30,8 @@ router
 
 router
   .get('/categories', validateToken, categoriesController.getCategories);
+
+router
+  .post('/post', validateBlogPost, validateToken, postController.createPost);
 
 module.exports = router;
