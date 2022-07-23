e16a2f37e6730e638013366fefba2d3a9daf88f7
matheusmattos7 <matheusm.mattos7@gmail.com>
2022-07-23 02:37:30 -0300

    feat: Adiciona a função getPostsById para postService.js

--- src/services/postService.js
+++ src/services/postService.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(config.development);
 
 const { BlogPost, PostCategory, Category, User } = require('../database/models');
 const { categoryIdsExist } = require('../middlewares/validateBlogpost');
+const { throwNotFound } = require('./_services');
 
 const createPost = async ({ title, content, categoryIds }, { id: userId }) => {
   const t = await sequelize.transaction();
@@ -43,7 +44,26 @@ const getPosts = async () => {
   return getAllPosts;
 };
 
+const getPostsById = async (id) => {
+  const getById = await BlogPost.findByPk(id, {
+    include: [
+      { model: User,
+        as: 'user',
+        attributes: { exclude: 'password' },
+      },
+      { model: Category, as: 'categories' },
+    ],
+  });
+
+  if (!getById) {
+    return throwNotFound('Post does not exist');
+  }
+
+  return getById;
+};
+
 module.exports = {
   createPost,
   getPosts,
+  getPostsById,
 };
